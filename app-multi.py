32db59060233c1717cf85adfcab79b56de79c82f
Michael Bell <jmbell91@gmail.com>
2023-08-04 15:08:28 -0500

    reverted to a single image paradigm to shift cuda/gpu errors to a later time - higher prioritiy to populate the historian than optimize for speed

--- app-multi.py
+++ app-multi.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# Objective: Run object detection on images from multiple cameras simultaneously
+# Hurdle: Error indicating that data is full precision but model is half when running on GPU
+
 import os
 import sys
 import cv2
@@ -17,7 +20,7 @@ import paho.mqtt.client as mqtt
 # these imports draw from yolov7, which is cloned when the dockerfiles are built
 from models.experimental import attempt_load
 from utils.general import non_max_suppression, check_img_size
-from utils.torch_utils import select_device, time_synchronized
+from utils.torch_utils import select_device
 
 from tracker.byte_tracker import BYTETracker
 
@@ -35,9 +38,9 @@ args = {
 
     'DEVICE': os.getenv("DEVICE", '0'),
 
-    # 'MODEL_NAME': os.getenv("MODEL_NAME", "yolov7-coco-bytetrack"),
-    # 'MODEL_VERSION': os.getenv("MODEL_VERSION", "0.1"),
-    # 'MODEL_ID': os.getenv("MODEL_ID", "abc123"),
+    'MODEL_NAME': os.getenv("MODEL_NAME", "yolov7-coco-bytetrack"),
+    'MODEL_VERSION': os.getenv("MODEL_VERSION", "0.1"),
+    'MODEL_ID': os.getenv("MODEL_ID", "abc123"),
 
     'WEIGHTS': os.getenv("WEIGHTS", "model.pt"),
     'AGNOSTIC_NMS': os.getenv("AGNOSTIC_NMS", ""),
@@ -68,7 +71,6 @@ logger.info("TΞꓘN01R")
 logger.info("TΞꓘN01R")
 
 logger.info(json.dumps(args))
-
 
 def error_str(rc):
     return '{}: {}'.format(rc, mqtt.error_string(rc))
@@ -165,120 +167,156 @@ names = model.module.names if hasattr(model, 'module') else model.names
 if half:
     model.half()  # to FP16
 
-if device.type != 'cpu':
-    model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(
-        next(model.parameters())))  # run once
-model.eval()
 
-tracker = BYTETracker(track_thresh=args["TRACKER_THRESHOLD"],
-                      match_thresh=args["TRACKER_MATCH_THRESHOLD"],
-                      track_buffer=args["TRACKER_BUFFER"],
-                      frame_rate=args["TRACKER_FRAME_RATE"])
+class ObjectTrackers:
+
+    def __init__(self, args):
+        self._trackers = {}
+        self.threshold = args["TRACKER_THRESHOLD"]
+        self.match_threshold = args["TRACKER_MATCH_THRESHOLD"]
+        self.buffer = args["TRACKER_BUFFER"]
+        self.frame_rate = args["TRACKER_FRAME_RATE"]
+
+    def _new_tracker(self):
+        # should we consider allowing the input payload to dynamically
+        # define the frame rate, etc. instead of assuming it here?
+        return BYTETracker(track_thresh=self.threshold,
+                           match_thresh=self.match_threshold,
+                           track_buffer=self.buffer,
+                           frame_rate=self.frame_rate)
+
+    def get(self, camera_id):
+        if camera_id not in self._trackers:
+            self._trackers[camera_id] = self._new_tracker()
+        return self._trackers[camera_id]
 
 
-def detect(img):
-    t0 = time_synchronized()
-    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
-    img = np.ascontiguousarray(img)
-    img = np.expand_dims(img, axis=0)
+logger.info("Tracking Model Initialization")
+trackers = ObjectTrackers(args)
 
-    img_tensor = torch.from_numpy(img).to(device)
-    img_tensor = img_tensor.half() if half else img_tensor.float()  # uint8 to fp16/32
-    img_tensor /= 255.0  # 0 - 255 to 0.0 - 1.0
 
+def detect(image_stack):
+    t0 = time.time()
     with torch.no_grad():
-        pred = model(img_tensor, augment=args["AUGMENTED_INFERENCE"])[0]
+        model_input = torch.from_numpy(image_stack).to(device)
+        model_input = model_input.half() if half else model_input.float()  # uint8 to fp16/32
+        model_input /= 255.0  # 0 - 255 to 0.0 - 1.0
+        model_output = model(model_input, augment=args["AUGMENTED_INFERENCE"])[0]
         detections = non_max_suppression(
-            pred,
+            model_output,
             args["CONF_THRESHOLD"],
             args["IOU_THRESHOLD"],
             args["CLASSES_TO_DETECT"],
             args["AGNOSTIC_NMS"])
-    inference_time = time_synchronized() - t0
+    inference_time = time.time() - t0
     logger.info("YOLOv7 Inference Time : {}".format(inference_time))
     return detections, inference_time
 
 
 def load_image(base64_image, userdata):
+    # preprocess of base64 string
     image_base64 = base64_image.split(',', 1)[-1]
+    # Decode the base64 string to byte64 array and read image
     image = Image.open(BytesIO(base64.b64decode(image_base64)))
-    # image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+    # Convert the BGR image to RGB
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+    # Resize an image to model size
     image = cv2.resize(image, (userdata["IMG_SIZE"], userdata["IMG_SIZE"]))
-    return image
+    return np.ascontiguousarray(image)
 
 
 def on_message(c, userdata, msg):
     try:
-        msg_time_0 = time_synchronized()
         message = str(msg.payload.decode("utf-8", "ignore"))
-        # {“timestamp”: “…”, “image”: <base64_mime>, “camera_id”: “A”, “camera_name”: “…”}
+        # {"images": [{“timestamp”: “…”, “image”: <base64_mime>, “camera_id”: “A”, “camera_name”: “…”}, ...]}
         try:
             data_received = json.loads(message)
         except json.JSONDecodeError as e:
             logger.error("Error decoding JSON:", e)
             sys.exit(1)
 
-        img = load_image(data_received["image"], userdata)
+        camera_images = data_received["images"]
 
-        detections, inference_time = detect(img)
+        images = []  # camera_image["image"] for camera_image in camera_images]
+        for camera_image in camera_images:
+            img = load_image(camera_image["image"], userdata)
+            images.append(img)
 
-        track_time_0 = time.time()
-        tracked_objects = tracker.update(torch.tensor(detections), img)
-        track_time = time.time() - track_time_0
+        # Represent list of images as a stack (tensor)
+        image_stack = np.transpose(
+            np.flip(np.stack(images), 3), (0, 3, 1, 2)).astype(np.float32) / 255.0
+        
+        image_stack = np.ascontiguousarray(image_stack)
 
-        msg_time_1 = time_synchronized()
+        detections, inference_time = detect(image_stack)
 
         payload = {
-            "timestamp": data_received["timestamp"],
-            "type": "objects",
-            "data": [],
-            "metadata": {
-                "applicaton": {
-                    "name": APP_NAME, 
-                    "version": "v1.0", 
-                    "processing_time": msg_time_1 - msg_time_0},
-                "peripheral": {
-                    "id": "00001", 
-                    "name": "parking-lot-1", 
-                    "type": "camera", 
-                    "image_height": img.shape[0], 
-                    "image_width": img.shape[1]},
-            },
+            # from registry eventually
+            "model": {"name": args["MODEL_NAME"],
+                      "version": args["MODEL_VERSION"],
+                      "id": args["MODEL_ID"]},
+            "inference_time": inference_time,
+            "results": []
         }
 
-        for tracked_object in tracked_objects:
-            x1 = tracked_object[0]
-            y1 = tracked_object[1]
-            x2 = tracked_object[2]
-            y2 = tracked_object[3]
-            track_id = tracked_object[4]
-            class_index = int(tracked_object[5])
-            score = tracked_object[6]
+        for i, (camera_image, detection) in enumerate(zip(camera_images, detections)):
 
-            payload["data"].append({
-                'trk_id': track_id,
-                'x1': x1,
-                'y1': y1,
-                'x2': x2,
-                'y2': y2,
-                'x_center': int((x1 + x2) / 2),
-                'y_center': int((y1 + y2) / 2),
-                'width': x2 - x1,
-                'height': y2 - y1,
-                'ratio': (y2 - y1) / (x2 - x1),
-                'score': score,
-                'area': x2 * y2,
-                'label': args["CLASS_NAMES"][class_index],
-                'track_time': track_time
-            })
+            result = {}
+
+            result["timestamp"] = data_received["timestamp"]
+
+            result["data"] = camera_image
+            result["data"]["width"] = images[i].shape[1]
+            result["data"]["height"] = images[i].shape[0]
+
+            camera_id = camera_image["camera_id"]
+            tracker = trackers.get(camera_id)
+            track_time_0 = time.time()
+            tracked_objects = tracker.update(
+                torch.tensor(detection), images[i])
+            track_time = time.time() - track_time_0
+
+            result["objects"] = []
+            for tracked_object in tracked_objects:
+                x1 = tracked_object[0]
+                y1 = tracked_object[1]
+                x2 = tracked_object[2]
+                y2 = tracked_object[3]
+                track_id = tracked_object[4]
+                class_index = int(tracked_object[5])
+                score = tracked_object[6]
+
+                result["objects"].append({
+                    'trk_id': track_id,
+                    'x1': x1,
+                    'y1': y1,
+                    'x2': x2,
+                    'y2': y2,
+                    'xc': int((x1 + x2) / 2),
+                    'yc': int((y1 + y2) / 2),
+                    'width': x2 - x1,
+                    'height': y2 - y1,
+                    'ratio': (y2 - y1) / (x2 - x1),
+                    'score': score,
+                    'area': x2 * y2,
+                    'label': args["CLASS_NAMES"][class_index],
+                    'track_time': track_time
+                })
+
+            payload["results"].append(result)
 
         msg = json.dumps(payload, cls=NumpyEncoder)
         client.publish(userdata['MQTT_OUT_0'], msg)
-        payload["image"] = "%s... - truncated for logs" % payload["image"][0:32]
-        logger.info(payload)
+
+        # for result in payload["results"]:
+        #     for obj in result["data"]:
+        #         if "image" in obj:
+        #             obj["image"]="{} - truncated for logs".format(
+        #                 obj["image"][:32])
+        # logger.debug(payload)
 
     except Exception as e:
-        logger.error(e)
+        logger.error('Error:', e)
         sys.exit(1)
 
 
