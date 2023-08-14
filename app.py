f29323cd1dd933ba91c35d208fb20aa355afe5d1
Michael Bell <jmbell91@gmail.com>
2023-08-14 17:01:44 -0500

    added missing import

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import numpy as np
 import paho.mqtt.client as mqtt
 from io import BytesIO
 from PIL import Image
+from tracker.byte_tracker import BYTETracker
 
 # these imports draw from yolov7, which is cloned when the dockerfiles are built
 from utils.datasets import letterbox
